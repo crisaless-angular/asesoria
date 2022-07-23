eecb634a10bfb0c369893fd77fbbb3e322bee577
crisaless <carlos@crisaless.es>
2022-07-23 15:58:53 +0200

    Crear recuperar contraseña diseño

---
+++ Controllers/RecuperarCuenta.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class RecuperarCuenta : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "¿Ha olvidado su contraseña?";
+            return View();
+        }
+    }
+}
